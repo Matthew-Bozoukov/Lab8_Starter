7921ff8572aaf6f4332c2552023e060b1b9e4030
Matthew-Bozoukov <matthewbozoukov@gmail.com>
2026-05-26 21:38:10 -0700

    here

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Lab8-Starter
+
+
+# Lab partners
+
+Matthew Bozoukov, only.
+
+# Question about graceful degradation
+Graceful degradation and service workers are related because service workers let a web app keep working when the network is slow or unavailable instead of failing completely. If we  cache the app shell and previously requested data, the service worker provides a usable experience.
+
+![PWA Screenshot](./pwa.png)
--- sw.js
+++ sw.js
@@ -2,6 +2,33 @@
 //         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-8-starter';
+const RECIPE_URLS = [
+  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+];
+const APP_SHELL_URLS = [
+  './',
+  './index.html',
+  './assets/styles/main.css',
+  './assets/scripts/main.js',
+  './assets/scripts/RecipeCard.js',
+  './manifest.json',
+  './assets/images/icons/0-star.svg',
+  './assets/images/icons/1-star.svg',
+  './assets/images/icons/2-star.svg',
+  './assets/images/icons/3-star.svg',
+  './assets/images/icons/4-star.svg',
+  './assets/images/icons/5-star.svg',
+  './assets/images/icons/arrow-down.png',
+  './assets/images/icons/icon-192x192.png',
+  './assets/images/icons/icon-256x256.png',
+  './assets/images/icons/icon-384x384.png',
+  './assets/images/icons/icon-512x512.png',
+];
 
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
@@ -9,7 +36,7 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      return cache.addAll([...APP_SHELL_URLS, ...RECIPE_URLS]);
     })
   );
 });
@@ -37,4 +64,18 @@ self.addEventListener('fetch', function (event) {
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
 });
